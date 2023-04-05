0be1ee627f177139627ec4622393009fe8dc4cac
nixonsmithhunter <nixonsmithhunter@gmail.com>
2023-04-05 11:50:48 -0700

    updated function to properly display and adjusted all graphs to update for aggregate functions

--- analysis/my_code/project_functions1.py
+++ analysis/my_code/project_functions1.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import numpy as np
 
-def load_and_process(df, columns_to_drop):
-    load_and_process = (
+def load_and_process(file_path, columns_to_drop):
+    df = pd.read_csv(file_path)  # Read the CSV file
+    processed_data = (
         df
+        .query('Year >= 2018 & Year <= 2021')  # Filter data for years 2018-2021
         .drop(columns=columns_to_drop)  # Drop unnecessary columns
-        .query("Year >= 2020 & Year < 2021")  # Filter data for years 2020
-        .groupby(['Year', 'Subject', 'Course'])  # Group data by year, subject, and course
+        .groupby(['Year', 'Campus', 'Subject', 'Course'])  # Group data by year, campus, subject, and course
         .agg({
             'Avg': ['mean', 'median'],  # Calculate the mean and median of the average grades
             'Median': ['mean', 'median'],  # Calculate the mean and median of the median grades
@@ -14,11 +15,15 @@ def load_and_process(df, columns_to_drop):
             'Percentile (75)': ['mean', 'median'],  # Calculate the mean and median of the 75th percentile
         })
         .round(0)  # Round the summary statistics to the nearest whole number
-        .sort_values(by=['Year', 'Subject', 'Course'])  # Sort the DataFrame by year, subject, and course
+        .sort_values(by=['Year', 'Campus', 'Subject', 'Course'])  # Sort the DataFrame by year, campus, subject, and course
         .reset_index()  # Reset the index
-        .fillna()  # Replace NaN values with 0
+        .fillna(0)  # Replace NaN values with 0
     )
-    return load_and_process
+    return processed_data
+
+
+
+
 
 
 
