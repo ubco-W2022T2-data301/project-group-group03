86c6b9d29e4f5e2ab3164c142c620af598e7cc02
YOUR NAME HERE <YOUR@EMAI>
2023-04-11 15:32:43 -0700

    f3

--- analysis/my_code/project_functions3.py
+++ analysis/my_code/project_functions3.py
@@ -15,6 +15,6 @@ def load_n_process(file_path):
         .reset_index(drop=True)  # Reset the index
         .rename(columns={'Avg': 'Average'})  # Rename the "Avg" column to "Average"
     )
-    new_df['Avg_25_percentile'] = df['Average'].apply(lambda x: x.quantile(0.25)).values
-    new_df['Avg_75_percentile'] = df['Average'].apply(lambda x: x.quantile(0.75)).values
+    new_df['Avg_25_percentile'] = df['Avg'].apply(lambda x: x.quantile(0.25)).values
+    new_df['Avg_75_percentile'] = df['Avg'].apply(lambda x: x.quantile(0.75)).values
     return new_df
