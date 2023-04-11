51b0041663bcbdef5c56d5ad2cded721943b66b2
YOUR NAME HERE <YOUR@EMAI>
2023-04-11 15:28:10 -0700

    f3

--- analysis/my_code/project_functions3.py
+++ analysis/my_code/project_functions3.py
@@ -1,22 +1,20 @@
 import pandas as pd
+import numpy as np
 
 def load_n_process(file_path):
     df = pd.read_csv(file_path)  # Read the CSV file 
-    grouped_df = df.drop(columns=['Section', 'Session']).groupby(['Year', 'Subject', 'Course'], as_index=False)
-    
-    summary_stats = grouped_df.agg({'Avg': ['mean', 'median']})
-    
-    # Calculate lower and upper quartiles
-    quartiles = grouped_df['Avg'].quantile([0.25, 0.75]).unstack().rename(columns={0.25: 'Q1', 0.75: 'Q3'})
-    
-    # Merge the summary statistics and quartiles DataFrames
-    new_df = pd.merge(summary_stats, quartiles, on=['Year', 'Subject', 'Course'])
-
     new_df = (
-        new_df.round(0)  # Round the summary statistics to the nearest whole number
+        df
+        .drop(columns=['Section', 'Session'])  # Drop unnecessary columns
+        .groupby(['Year', 'Subject', 'Course'], as_index=False)  # Group data by year, subject, course, title, and professor
+        .agg({
+            'Avg': ['mean', 'median'],  # Calculate the mean and median of the average grades
+        })
+        .round(0)  # Round the summary statistics to the nearest whole number
         .sort_values(by=['Year', 'Subject', 'Course'])  # Sort the DataFrame by year, subject, and course
         .reset_index(drop=True)  # Reset the index
+        .rename(columns={'Avg': 'Average'})  # Rename the "Avg" column to "Average"
     )
-    new_df.columns = ['Year', 'Subject', 'Course', 'Average_mean', 'Average_median', 'Average_Q1', 'Average_Q3']
-
-    return new_df
+    new_df['Avg_25_percentile'] = df['Average'].apply(lambda x: x.quantile(0.25)).values
+    new_df['Avg_75_percentile'] = df['Average'].apply(lambda x: x.quantile(0.75)).values
+return new_df
