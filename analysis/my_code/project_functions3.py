a9d612530c2b3ea9c72577966b5d85be117c53a3
YOUR NAME HERE <YOUR@EMAI>
2023-04-11 15:41:26 -0700

    f3

--- analysis/my_code/project_functions3.py
+++ analysis/my_code/project_functions3.py
@@ -3,12 +3,10 @@ import numpy as np
 
 def load_and_process(file_path):
     df = pd.read_csv(file_path)
-    # Drop unnecessary columns
-    df = df.drop(columns=columns_to_drop)
 
-    # Calculate the new columns based on the 'Avg' column
     new_df = (
         df
+        .drop(columns=['Section', 'Session'])  
         .groupby(['Year','Campus','Subject','Course'])
         .agg(Avg_mean=('Avg', 'mean'),
              Avg_median=('Avg', 'median'))
@@ -18,9 +16,8 @@ def load_and_process(file_path):
         .fillna(0)
     )
 
-    # Calculate the 25th and 75th percentiles
-    new_df['Avg_25_percentile'] = df['Avg'].apply(lambda x: x.quantile(0.25)).values
-    new_df['Avg_75_percentile'] = df['Avg'].apply(lambda x: x.quantile(0.75)).values
+    new_df['Lower Quartile'] = df['Avg'].apply(lambda x: x.quantile(0.25)).values
+    new_df['Upper Quartile'] = df['Avg'].apply(lambda x: x.quantile(0.75)).values
 
     return new_df
 
