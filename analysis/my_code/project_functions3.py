f4c7f40fcf7cc56d02f280b00c9c0375ec3425f8
YOUR NAME HERE <YOUR@EMAI>
2023-04-11 15:22:13 -0700

    f3

--- analysis/my_code/project_functions3.py
+++ analysis/my_code/project_functions3.py
@@ -1,7 +1,4 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 def load_n_process(file_path):
     df = pd.read_csv(file_path)  # Read the CSV file 
@@ -10,13 +7,20 @@ def load_n_process(file_path):
         .drop(columns=['Section', 'Session'])  # Drop unnecessary columns
         .groupby(['Year', 'Subject', 'Course'], as_index=False)  # Group data by year, subject, course, title, and professor
         .agg({
-            'Avg': ['mean', 'median'],  # Calculate the mean and median of the average grades
+            'Avg': ['mean', 'median', 'quantile'],  # Calculate the mean, median, and quantiles of the average grades
         })
         .round(0)  # Round the summary statistics to the nearest whole number
         .sort_values(by=['Year', 'Subject', 'Course'])  # Sort the DataFrame by year, subject, and course
         .reset_index(drop=True)  # Reset the index
         .rename(columns={'Avg': 'Average'})  # Rename the "Avg" column to "Average"
     )
-    new_df['Lower Quartile'] = df['Avg'].apply(lambda x: np.quantile(x, 0.25)).values
-    new_df['Upper Quartile'] = df['Avg'].apply(lambda x: np.quantile(x, 0.75)).values
+    
+    # Calculate the lower and upper quartiles
+    new_df[('Average', 'Q1')] = new_df[('Average', 'quantile')].apply(lambda x: x(0.25))
+    new_df[('Average', 'Q3')] = new_df[('Average', 'quantile')].apply(lambda x: x(0.75))
+
+    # Drop the 'quantile' column
+    new_df = new_df.drop(columns=[('Average', 'quantile')])
+
     return new_df
+
